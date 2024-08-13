895a8976e229c25eca5353c127d0471b38d0b6cd
MaelMbange <maelmbange21@gmail.com>
2024-08-13 23:03:00 +0200

    correction of the condition -> chanegd after to before

--- lib/src/api_key.dart
+++ lib/src/api_key.dart
@@ -21,7 +21,7 @@ class ApiKey {
   int get ratelimitRemaining => _ratelimitRemaining;
   bool get isValid =>
       _isvalid &&
-      (_ratelimitRemaining > 0 || _retryAfter.isAfter(DateTime.now())) &&
+      (_ratelimitRemaining > 0 || _retryAfter.isBefore(DateTime.now())) &&
       _userID.isNotEmpty;
 
   DateTime get retryAfter => _retryAfter;
